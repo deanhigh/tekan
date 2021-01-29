ff8dd871f3ab344c39d3c97a930f036785c9e196
dean.high <deanhigh@users.noreply.github.com>
2021-01-29 21:34:39 +0000

    Graph, simple

--- ui/static/app/time-series/time-series.component.js
+++ ui/static/app/time-series/time-series.component.js
@@ -1,13 +1,22 @@
 'use strict';
 
-angular.
-module('timeSeries').
-component('timeSeries', {
+// angular.
+// module('timeSeries').factory('TimeSeries',
+//     function ($resource) {
+//         return $resource('/api/time-series/:ticker');
+//     });
+
+
+angular.module('timeSeries').component('timeSeries', {
     templateUrl: 'app/time-series/time-series.template.html',
-    controller: function TimeSeriesController($scope) {
-        console.log($scope);
+    controller: function TimeSeriesController($scope, $http) {
         this.loadTimeSeries = function () {
-            console.log($scope);
         };
+
+        //console.log(TimeSeries.query());
+        $http.get("/api/time-series")
+            .success(function (response) {
+                new Dygraph(document.getElementById("primaryTimeSeriesGraph"), response);
+            });
     }
 });
--- ui/static/app/time-series/time-series.module.js
+++ ui/static/app/time-series/time-series.module.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('timeSeries', []);
+angular.module('timeSeries', ['ngResource']);
